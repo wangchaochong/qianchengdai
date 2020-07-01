1f0eaec48cb11981860e6b726f8145efaf54ae5e
wangchaochong <819609179@qq.com>
2020-07-01 10:39:11 +0800

    修改了测试报告的名称

--- run_test.py
+++ run_test.py
@@ -11,12 +11,12 @@ loader = unittest.TestLoader()
 
 # 加载所有的测试用例
 cases_suit = loader.discover(config.CASE_PATH)
-report_name = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-report_file = os.path.join(config.REPORT_PATH, "测试报告"+report_name+".html")
+# report_name = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+report_file = os.path.join(config.REPORT_PATH, "前程贷接口测试报告.html")
 # 获取HTML测试报告
 if __name__ == '__main__':
     with open(report_file, "wb") as f:
         runner = HTMLTestRunner(f, title="python自动化测试报告", description="测试详情", tester="小汪")
         runner.run(cases_suit)
-        SendMailWithReport.send_mail(**Handler.yaml_data["email_address"], file_name=report_file)
-        print("邮件发送成功")
+        # SendMailWithReport.send_mail(**Handler.yaml_data["email_address"], file_name=report_file)
+        # print("邮件发送成功")
